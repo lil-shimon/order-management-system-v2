3162acb2442e3fddf621871c27665faec236058e
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-04-26 21:42:51 +0900

    feat: 🎸 firebase function deploy

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,27 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+export const setCustomClaims = functions.auth.user().onCreate(async (user) => {
+  // for hasura endpoints
+  const customClaims = {
+    "https://hasura.io/jwt/claims": {
+      "x-hasura-default-role": "staff",
+      "x-hasura-allowed-roles": ["staff"],
+      "x-hasura-user-id": user.uid,
+    },
+  };
+
+  try {
+    // adapt custom claim
+    await admin.auth().setCustomUserClaims(user.uid, customClaims);
+
+    // notice to react app when finish custom claims
+    await admin.firestore().collection("user_meta").doc(user.uid).create({
+      refreshTime: admin.firestore.FieldValue.serverTimestamp(),
+    });
+  } catch (e) {
+    console.log(e);
+  }
+});
