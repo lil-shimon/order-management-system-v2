ca21c2a40439e9ea3232a721f2de142f6fffc50e
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-04-29 22:55:17 +0900

    feat: 🎸 added custom hook for firebase auth

---
+++ hooks/useFirebaseAuth.ts
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2022. Kenta Shimosawa
+ */
+
+
+import { useState, useCallback, ChangeEvent, FormEvent } from "react";
+import firebase from "../firebaseConfig";
+
+export const useFirebaseAuth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLogin, setIsLogin] = useState(true);
+
+  const emailChange = useCallback((e: ChangeEvent<HTMLFormElement>) => {
+    setEmail(e.target.value);
+  }, []);
+
+  const passwordChange = useCallback((e: ChangeEvent<HTMLFormElement>) => {
+    setPassword(e.target.value);
+  }, []);
+
+  const resetInput = useCallback(() => {
+    setEmail("");
+    setPassword("");
+  }, []);
+
+  const toggleMode = useCallback(() => {
+    setIsLogin(!isLogin);
+  }, [isLogin]);
+
+  /**
+   * ログイン時はメールアドレスとパスワードでログイン
+   * 未ログイン時は新規ユーザー追加
+   * @type {(e: React.FormEvent<HTMLFormElement>) => Promise<void>}
+   */
+  const authUser = useCallback(async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (isLogin) {
+      try {
+        await firebase.auth().signInWithEmailAndPassword(email, password);
+      } catch ({ message }) {
+        alert(message);
+      }
+      resetInput();
+    } else {
+      try {
+        await firebase.auth().createUserWithEmailAndPassword(email, password);
+      } catch ({ message }) {
+        alert(message);
+      }
+      resetInput();
+    }
+  }, [email, password, isLogin]);
+
+  return {
+    email, password, emailChange, passwordChange, resetInput, isLogin, toggleMode, authUser,
+  };
+};
--- hooks/useUserChanged.ts
+++ hooks/useUserChanged.ts
@@ -12,69 +12,69 @@ import Cookie from "universal-cookie";
 export let unSubMeta: () => void;
 
 export const useUserChanged = () => {
-	const cookie = new Cookie();
-	const router = useRouter();
-	const HASURA_TOKEN_KEY = "https//hasura.io/jwt/claims";
+  const cookie = new Cookie();
+  const router = useRouter();
+  const HASURA_TOKEN_KEY = "https//hasura.io/jwt/claims";
 
-	useEffect(() => {
-		// ユーザー情報が変化
-		// 変化したユーザー情報がuserとして利用可能
-		const unSubUser = firebase.auth().onAuthStateChanged(async (user) => {
-			if (user) {
-				// ユーザーに対応したJWTトークンを取得
-				const token = await user.getIdToken(true);
+  useEffect(() => {
+    // ユーザー情報が変化
+    // 変化したユーザー情報がuserとして利用可能
+    const unSubUser = firebase.auth().onAuthStateChanged(async (user) => {
+      if (user) {
+        // ユーザーに対応したJWTトークンを取得
+        const token = await user.getIdToken(true);
 
-				// Token結果を取得
-				const idTokenResult = await user.getIdTokenResult();
+        // Token結果を取得
+        const idTokenResult = await user.getIdTokenResult();
 
-				// カスタムクレイムの内容を取得
-				// setCustomClaimがしっかり適応されている場合は取得できる
-				// x-hasura-allowed-roles
-				// x-hasura-default-role
-				// x-hasura-user-id
-				const hasuraClaims = idTokenResult.claims[HASURA_TOKEN_KEY];
+        // カスタムクレイムの内容を取得
+        // setCustomClaimがしっかり適応されている場合は取得できる
+        // x-hasura-allowed-roles
+        // x-hasura-default-role
+        // x-hasura-user-id
+        const hasuraClaims = idTokenResult.claims[HASURA_TOKEN_KEY];
 
-				// しっかりcustomClaimの値が取得できている場合は取得したjwtTokenをcookieにセットし
-				// ページへリダイレクトさせる
-				if (hasuraClaims) {
-					cookie.set("token", token, { path: "/" });
-					await router.push("/tasks");
-				}
-				// setCustomUserClaimが完了していない時
-				if (!hasuraClaims) {
+        // しっかりcustomClaimの値が取得できている場合は取得したjwtTokenをcookieにセットし
+        // ページへリダイレクトさせる
+        if (hasuraClaims) {
+          cookie.set("token", token, { path: "/" });
+          await router.push("/tasks");
+        }
+        // setCustomUserClaimが完了していない時
+        if (!hasuraClaims) {
 
-					// userMetaの値が変化するのをモニタリングする
-					const userRef = firebase
-						.firestore()
-						.collection("user_meta")
-						.doc(user.uid);
+          // userMetaの値が変化するのをモニタリングする
+          const userRef = firebase
+            .firestore()
+            .collection("user_meta")
+            .doc(user.uid);
 
-					// 変化があった時実行
-					unSubMeta = userRef.onSnapshot(async () => {
-						// スナップショットからその時最新のtokenを取得
-						const tokenSnap = await user.getIdToken(true);
+          // 変化があった時実行
+          unSubMeta = userRef.onSnapshot(async () => {
+            // スナップショットからその時最新のtokenを取得
+            const tokenSnap = await user.getIdToken(true);
 
-						// スナップショットからその時最新の結果を取得
-						const idTokenResultSnap = await user.getIdTokenResult();
+            // スナップショットからその時最新の結果を取得
+            const idTokenResultSnap = await user.getIdTokenResult();
 
-						// CustomClaimが適応されているかを判定するため取得
-						const hasuraClaimsSnap = idTokenResultSnap.claims[HASURA_TOKEN_KEY];
+            // CustomClaimが適応されているかを判定するため取得
+            const hasuraClaimsSnap = idTokenResultSnap.claims[HASURA_TOKEN_KEY];
 
-						if (hasuraClaimsSnap) {
-							// しっかりcustomClaimの値が取得できている場合は取得したjwtTokenをcookieにセットし
-							// ページへリダイレクトさせる
-							cookie.set("token", tokenSnap, { path: "/" });
-							await router.push("/tasks");
-						}
-					});
-				}
-			}
-		});
-		return () => {
-			unSubUser();
-		};
-	}, []);
+            if (hasuraClaimsSnap) {
+              // しっかりcustomClaimの値が取得できている場合は取得したjwtTokenをcookieにセットし
+              // ページへリダイレクトさせる
+              cookie.set("token", tokenSnap, { path: "/" });
+              await router.push("/tasks");
+            }
+          });
+        }
+      }
+    });
+    return () => {
+      unSubUser();
+    };
+  }, []);
 
 
-	return {};
+  return {};
 };
