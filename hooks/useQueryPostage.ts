a97d3106284c1848d8d9e3c2fd16addb28596d27
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-05-05 15:13:49 +0900

    feat: 🎸 postage create

--- hooks/useQueryPostage.ts
+++ hooks/useQueryPostage.ts
@@ -6,6 +6,9 @@ import { Postage } from "../types/types";
 import { endpoint } from "../utils/api";
 import { request } from "graphql-request";
 import { GET_POSTAGES } from "../queries/queries";
+import { useRouter } from "next/router";
+import { useDispatch } from "react-redux";
+import { resetPostage } from "../slicers/documentSlicer";
 
 interface PostageRes {
   postages: Postage[];
@@ -15,4 +18,18 @@ export const fetchPostage = async () => {
   // @ts-ignore
   const { postages: data } = await request<PostageRes>(endpoint, GET_POSTAGES);
   return data;
+};
+
+export const useQueryPostage = () => {
+  const router = useRouter();
+  const dispatch = useDispatch();
+
+  const handleMoveToCreate = () => {
+    dispatch(resetPostage());
+    router.push("/postage/create");
+  };
+
+  return {
+    handleMoveToCreate,
+  };
 };
